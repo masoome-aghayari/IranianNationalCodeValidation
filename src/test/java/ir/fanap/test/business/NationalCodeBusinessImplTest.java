a645e687f5529858d74a2f415e22469e22a6e995
aghayari <masoome.aghayari@yahoo.com>
2021-04-11 19:34:55 +0430

    NationalCodeBusinessImplTest updated

--- src/test/java/ir/fanap/test/business/NationalCodeBusinessImplTest.java
+++ src/test/java/ir/fanap/test/business/NationalCodeBusinessImplTest.java
@@ -4,13 +4,21 @@ import ir.fanap.business.NationalCodeBusinessImpl;
 import ir.fanap.model.NationalCode;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import org.mockito.Mockito;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NationalCodeBusinessImplTest {
     NationalCodeBusinessImpl nationalCodeBusiness;
+
+    @DataProvider
+    public static Object[][] nationalCodeValidatorDataProvider() {
+        return new Object[0][];
+    }
 
     @BeforeClass
     public void setup() {
@@ -18,30 +26,63 @@ public class NationalCodeBusinessImplTest {
     }
 
     @DataProvider(name = "repeatedDigitsNationalCodeDataProvider")
-    public NationalCode repeatedDigitsNationalCodeDataProvider() {
-        return new NationalCode("1111111111", LocalDateTime.now());
+    public Object[][] repeatedDigitsNationalCodeDataProvider() {
+        NationalCode repeatedDigitsNationalCode = new NationalCode("1111111111", LocalDateTime.now());
+        return new Object[][]{{repeatedDigitsNationalCode}};
     }
 
     @DataProvider(name = "eightDigitNationalCodeDataProvider")
-    public NationalCode eightDigitNationalCodeDataProvider() {
-        return new NationalCode("25369685", LocalDateTime.now());
+    public Object[][] eightDigitNationalCodeDataProvider() {
+        NationalCode eightDigitsNationalCode = new NationalCode("25369685", LocalDateTime.now());
+        return new Object[][]{{eightDigitsNationalCode}};
     }
 
     @DataProvider(name = "nineDigitNationalCodeDataProvider")
-    public NationalCode nineDigitNationalCodeDataProvider() {
-        return new NationalCode("253696857", LocalDateTime.now());
+    public Object[][] nineDigitNationalCodeDataProvider() {
+        NationalCode nineDigitsNationalCode = new NationalCode("253696857", LocalDateTime.now());
+        return new Object[][]{{nineDigitsNationalCode}};
     }
 
     @DataProvider(name = "correctNationalCodeDataProvider")
-    public NationalCode correctNationalCodeDataProvider() {
-        return new NationalCode("4310928838", LocalDateTime.now());
+    public Object[][] correctNationalCodeDataProvider() {
+        NationalCode correctNationalCode = new NationalCode("4310928838", LocalDateTime.now());
+        return new Object[][]{{correctNationalCode}};
+    }
+
+    @DataProvider(name = "calculateLastDigitDataProvider")
+    public Object[][] calculateLastDigitDataProvider() {
+        String nationalCodeString1 = "4310928838";
+        String nationalCodeString2 = "588927678512";
+        String nationalCodeString = "1111111111";
+        String nationalCodeString = "1234567";
+        return new Object[][]{{correctNationalCode}};
     }
 
     @Test(dataProvider = "correctNationalCodeDataProvider")
-    public void testCalculateLastDigit(NationalCode nationalCode) {
-        int actualResult = nationalCodeBusiness.calculateLastDigit(nationalCode.getNationalCode());
-        int expectedResult = nationalCode.getNationalCode().charAt(9) - 48;
+    public void testCalculateLastDigit(NationalCode nationalCode, int expectedResult) {
+        int actualResult = nationalCodeBusiness.calculateNationalCodeLastDigit(nationalCode.getNationalCode());
         Assert.assertEquals(actualResult, expectedResult);
     }
-}
 
+    @Test(dataProvider = "nationalCodeValidatorDataProvider")
+    public void testValidateNationalCode(List<NationalCode> expectedResult) {
+        expectedResult.forEach(nationalCode -> nationalCodeBusiness.validateNationalCode(nationalCode));
+        List<NationalCode> actualResult = nationalCodeBusiness.getNationalCodes();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(dataProvider = "nationalCodeValidatorDataProvider")
+    public void testGetStateCode(List<NationalCode> expectedResult) {
+        List<String> actualResult = new ArrayList<>();
+        expectedResult.forEach(nationalCode -> actualResult.add(nationalCodeBusiness.getStateCode(nationalCode)));
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(dataProvider = "nationalCodeValidatorDataProvider")
+    public void testGetPersonUniqueCode(List<NationalCode> expectedResult) {
+        List<String> actualResult = new ArrayList<>();
+        expectedResult.forEach(nationalCode -> actualResult.add(nationalCodeBusiness.getStateCode(nationalCode)));
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+}
