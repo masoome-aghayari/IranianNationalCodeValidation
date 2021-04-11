03fdca30ff0d26f6437c459da3f540f582d3a1a1
aghayari <masoome.aghayari@yahoo.com>
2021-04-11 11:43:39 +0430

    class NationalCode added

---
+++ src/main/java/NationalCode.java
@@ -0,0 +1,48 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class NationalCode {
+    private String nationalCode;
+    private boolean isValid;
+    private LocalDateTime inputTime;
+
+    public NationalCode(String userInput, LocalDateTime inputTime) {
+        setNationalCode(userInput);
+        setInputTime(inputTime);
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        int length = nationalCode.length();
+        this.nationalCode = length == 8 ? "00" + nationalCode :
+                length == 9 ? "0" + nationalCode : nationalCode;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+
+    public LocalDateTime getInputTime() {
+        return inputTime;
+    }
+
+    public void setInputTime(LocalDateTime inputTime) {
+        this.inputTime = inputTime;
+    }
+
+    @Override
+    public String toString() {
+        return "NationalCode{" +
+                "nationalCode='" + nationalCode + '\'' +
+                ", inputDateTime=" + inputTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) +
+                ", isValid=" + isValid +
+                '}';
+    }
+}
